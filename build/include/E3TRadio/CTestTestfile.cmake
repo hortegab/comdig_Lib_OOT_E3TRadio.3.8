8799b304b01e5d3d0d9821ece2d467a00e710076
UIS-E3T <comdiguis@saber.uis.edu.co>
2020-06-21 20:17:07 -0500

    first

--- build/include/E3TRadio/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cloudmanager/MisGNURadio/MisGits/comdig_Lib_OOT_E3TRadio_python3/include/E3TRadio
-# Build directory: /home/cloudmanager/MisGNURadio/MisGits/comdig_Lib_OOT_E3TRadio_python3/build/include/E3TRadio
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
