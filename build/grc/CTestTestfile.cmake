3f7e3fc8766ccd1fb280d517e9994fea74d4bb94
UIS-E3T <comdiguis@saber.uis.edu.co>
2020-06-21 19:54:08 -0500

    primera version

---
+++ build/grc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cloudmanager/MisGNURadio/MisGits/comdig_Lib_OOT_E3TRadio_python3/grc
+# Build directory: /home/cloudmanager/MisGNURadio/MisGits/comdig_Lib_OOT_E3TRadio_python3/build/grc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/python/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cloudmanager/MisGNURadio/MisGits/comdig_Lib_OOT_E3TRadio_python3/python
+# Build directory: /home/cloudmanager/MisGNURadio/MisGits/comdig_Lib_OOT_E3TRadio_python3/build/python
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
